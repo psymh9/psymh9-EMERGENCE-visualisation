4345081ce9e1a05306a598f1415a14341fd67152
Maximus Hughes-Gray <psymh9@nottingham.ac.uk>
2023-08-22 13:02:18 +0100

    Fix FNF Error

--- NetworkVisualisationTool.py
+++ NetworkVisualisationTool.py
@@ -38,7 +38,7 @@ class Misc(Values):
    ERROR_MESSAGE = ValueConstant('Some error occured while pushing the code')
    PATH_OF_GIT_REPO = ValueConstant(r'')
    COMMIT_MESSAGE = ValueConstant('New members added to the dataset')
-   EXCEL_FILE = ValueConstant("NetworkVisualisationData\\EMERGENCECollatedData.xlsm")
+   EXCEL_FILE = ValueConstant("NetworkVisualisationData\\EMERGENCECollatedData.xlsx")
    ZERO = ValueConstant(0)
    ONE = ValueConstant(1)
    TWO = ValueConstant(2)
